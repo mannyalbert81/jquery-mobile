a8956139d42ca734d2a20f1e09dcb9423c2637d1
dannymasoft <danny@masoft.net>
2017-09-19 12:12:57 -0500

    reg

--- www/js/registrar.js
+++ www/js/registrar.js
@@ -1,13 +1,17 @@
 /***declaracion variables***/
-var base_url = 'http://vademano.masoft.net/webservices/';
-var pag_service = 'afiliadoService.php'
+//var base_url = 'http://vademano.masoft.net/';
+//var pag_service = 'afiliadoService.php'
+var base_url = 'http://186.4.203.42:4000/vademano/webservices/';
+var pag_service = 'afiliadoService.php' ;
+//var base_url = 'http://localhost:5000/Vademano/';
+//var pag_service = 'index.php?controller=Afiliaciones&action=registraAfiliadoMobil' ;
 
 
 $(document).on("ready",ini);
 
 function ini()
 {	
-	$('#parent').on('click', '#child',afiliar);
+	$('#btnRegistrar').click(afiliar);
 }
 
 function afiliar()
@@ -15,16 +19,29 @@ function afiliar()
 	$.ajax({
 		   type: 'POST',
 		   url: base_url+pag_service,
-		   data:{action:'insertar'}, 
-		   contentType: 'application/json; utf-8',
+		   data:{action:'insertar'},
 		   dataType: 'json',
 		   success: function (data) {
-		       if (data.d != null) {
-		         alert('en esta seccion se muestra el resultado: '+data.d);
-		     }
+			   console.log('llego');
+			   //console.log(data.listaUsuario[0].nombre);
 		  },
 		   error: function (jqXHR, textStatus, errorThrown) {
 		 }
 
 		});
 }
+
+/*
+$.ajax({
+type: 'POST',
+url: base_url+pag_service,
+data:{'action':'insertar'},
+dataType: 'json',
+success: function (data) {
+	   console.log('llego');
+	   console.log(data.listaUsuario[0].nombre);
+},
+error: function (jqXHR, textStatus, errorThrown) {
+}
+
+});*/
