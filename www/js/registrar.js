9b26e68adb1cc76d4cd330c93b6a71da2780eb82
dannymasoft <danny@masoft.net>
2017-09-19 12:23:15 -0500

    asd

--- www/js/registrar.js
+++ www/js/registrar.js
@@ -1,10 +1,12 @@
 /***declaracion variables***/
 //var base_url = 'http://vademano.masoft.net/';
 //var pag_service = 'afiliadoService.php'
-var base_url = 'http://186.4.203.42:4000/vademano/webservices/';
-var pag_service = 'afiliadoService.php' ;
+//var base_url = 'http://186.4.203.42:4000/vademano/webservices/';
+//var pag_service = 'afiliadoService.php' ;
 //var base_url = 'http://localhost:5000/Vademano/';
 //var pag_service = 'index.php?controller=Afiliaciones&action=registraAfiliadoMobil' ;
+var base_url = 'http://127.0.0.1:5000/vademano/webservices/';
+var pag_service = 'afiliadoService.php' ;
 
 
 $(document).on("ready",ini);
