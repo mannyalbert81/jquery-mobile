15afe618acbdf9664eed800c7e2b5edf03bfcf47
dannymasoft <danny@masoft.net>
2017-09-20 14:42:28 -0500

    try

--- www/js/ficha.js
+++ www/js/ficha.js
@@ -1,31 +1,52 @@
-
-var base_url = 'http://localhost:4000/Vademano/webservices/';
+var base_url = 'http://localhost:5000/Vademano/webservices/';
 var pag_service = 'FichaService.php' ;
-
+var fichas = null;
 
 $(document).on("ready",ini);
 
 function ini()
 {
-	info();
+	
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
-var db1 = window.openDatabase("vade.db", "1.0", "MY DB", 200000); //will create database Dummy_DB or open it
 
+var db = window.openDatabase("vade.db", "1.0", "MY DB", 200000); //crea o abre la base
 
-function onDeviceReady() {
-
-	db1.transaction(info, errorCB, successCB);
+function onDeviceReady() 
+{
+	db.transaction(init_pag, errorCB, successCB);
 }
 
-function info(tx1)
+function init_pag(tx)
+{
+	tx.executeSql("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT , nombres_usuarios TEXT NOT NULL, apellidos_usuarios TEXT NOT NULL, usuario_usuarios TEXT NOT NULL, clave_usuarios TEXT NOT NULL);");
+	tx.executeSql("DELETE FROM usuarios;");
+}
+
+function traeFichas()
+{
+	
+}
+
+
+function info(tx)
 {
 	
 	
 	//tx.executeSql('CREATE TABLE IF NOT EXISTS fichas_service (id_fichas_service INTEGER PRIMARY KEY AUTOINCREMENT, id_fichas INTEGER NOT NULL, nombre_fichas TEXT NOT NULL, encabezado_tabla_fichas TEXT NOT NULL, farmacocinetica_fichas TEXT NOT NULL, accion_terapeutica_fichas TEXT NOT NULL, clasificacion_farmacologica_fichas TEXT NOT NULL, forma_terapeutica_fichas TEXT NOT NULL, indicaciones_uso_fichas TEXT NOT NULL, interacciones_fichas TEXT NOT NULL, contraindicaciones_fichas TEXT NOT NULL, periodo_retiro_fichas TEXT NOT NULL, advertencias_fichas TEXT NOT NULL, presentacion_fichas TEXT NOT NULL, registro_sanitario_fichas TEXT NOT NULL, id_fichas_fotos INTEGER NOT NULL, consultas_fichas INTEGER NOT NULL, buscador TEXT NOT NULL, mecanismo_accion_fichas TEXT NOT NULL, efectos_colaterales_fichas TEXT NOT NULL, conservacion_fichas TEXT NOT NULL, ingredientes_fichas TEXT NOT NULL, tipo_alimento_fichas TEXT NOT NULL, encabezado_dosificacion_fichas TEXT NOT NULL, tipo_ficha TEXT NOT NULL, tabla_formas_administracion TEXT NOT NULL, tabla_laboratorios TEXT NOT NULL, tabla_distribuidores TEXT NOT NULL, tabla_composicion TEXT NOT NULL, tabla_dosificacion TEXT NOT NULL)');
-	tx1.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT , nombres_usuarios TEXT NOT NULL, apellidos_usuarios TEXT NOT NULL, usuario_usuarios TEXT NOT NULL, clave_usuarios TEXT NOT NULL)');
+	tx.executeSql("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT , nombres_usuarios TEXT NOT NULL, apellidos_usuarios TEXT NOT NULL, usuario_usuarios TEXT NOT NULL, clave_usuarios TEXT NOT NULL)");
 	//tx.executeSql("DELETE FROM usuarios");
+	
+	var _nombres_usuarios="Maycol Flores";
+	var _apellidos_usuarios ="Rosabal Gutiérrez";
+	var _usuario_usuarios="JASON";
+	var _clave_usuarios="JASON";
+	
+    var executeQuery = "INSERT INTO usuarios(nombres_usuarios,apellidos_usuarios, usuario_usuarios, clave_usuarios) VALUES (?,?,?,?)";
+    
+    tx.executeSql(executeQuery, [ _nombres_usuarios,  _apellidos_usuarios, _usuario_usuarios, _clave_usuarios],insertado,errIns);
+    
 	$.ajax({
 		   type: 'POST',
 		   url: base_url+pag_service,
@@ -108,21 +129,7 @@ function info(tx1)
 				    });
 				  }*/
 				   
-				   var _id = 1;
-					var _nombres_usuarios="Maycol Flores";
-					var _apellidos_usuarios ="Rosabal Gutiérrez";
-					var _usuario_usuarios="JASON";
-					var _clave_usuarios="JASON";
-					
-				    var executeQuery = "INSERT INTO usuarios(nombres_usuarios,apellidos_usuarios, usuario_usuarios, clave_usuarios) VALUES (?,?,?,?)";
-				    
-				    tx1.executeSql(executeQuery, [ _nombres_usuarios,  _apellidos_usuarios, _usuario_usuarios, _clave_usuarios],
-				    	function(tx, result) {
-				    		//alert('Inserted');
-				    	},
-				    	function(error){
-				    		alert('Error al cargar Usuarios');
-				    });
+				   
 				    
 			   }
 			    show_fichas();
@@ -138,7 +145,7 @@ function info(tx1)
 
 
 function show_fichas(){
-	db1.transaction(function(transaction) {
+	db.transaction(function(transaction) {
 	transaction.executeSql('SELECT * FROM usuarios', [], function (tx1, results) {
 	var id = "";
 	var nombre = "";
@@ -175,7 +182,10 @@ function errorCB(err) {
 
 //function will be called when process succeed
 function successCB() {
-    alert("!zvxv");
-    show_fichas();
+    //show_fichas();
     //db.transaction(queryDB,errorCB);
 }
+function insertado(tx,result)
+{ console.log(result+'\n'+'insertado');}
+function errIns(error)
+{console.log(error);}
