356fbbd33f4f8170735b254c6f1bbf50d30b12bb
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-09-22 10:03:31 -0500

    c cv

--- www/js/database.js
+++ www/js/database.js
@@ -19,11 +19,9 @@ function onDeviceReady() {
 
 //create table and insert some record
 function populateDB(tx) {
-
 	
 	//creamos  las tablas
-	tx.executeSql('CREATE TABLE IF NOT EXISTS 
-			usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT , nombres_usuario TEXT, 	apellidos_usuario TEXT, usuario_usuarios TEXT, nombre_estado TEXT)');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT, nombres_usuario TEXT, apellidos_usuario TEXT, usuario_usuario TEXT, clave_usuario TEXT)');
 	
 					
 	var _id = 1;
@@ -32,9 +30,9 @@ function populateDB(tx) {
 	var _usuario_usuarios="JASON";
 	var _clave_usuarios="JASON";
 	
-    var executeQuery = "INSERT INTO usuarios(nombres_usuario,apellidos_usuario, usuario_usuario) VALUES (?,?,?,?)";
+    var executeQuery = "INSERT INTO usuarios(nombres_usuario,apellidos_usuario, usuario_usuario, clave_usuario) VALUES (?,?,?,?)";
     
-    tx.executeSql(executeQuery, [ _nombres_usuarios,  _apellidos_usuarios, _usuario_usuarios],
+    tx.executeSql(executeQuery, [ _nombres_usuarios,  _apellidos_usuarios, _usuario_usuarios, _clave_usuarios],
     	function(tx, result) {
     		//alert('Inserted');
     	},
